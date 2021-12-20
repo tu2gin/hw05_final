45213d968c521fefd237d21e2a5540a658ce8ba9
tu2gin <92797440+tu2gin@users.noreply.github.com>
2021-12-20 14:55:09 +0300

    test5

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -13,14 +13,6 @@ class StaticURLHomepageTests(TestCase):
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, settings.OK)
 
-    def test_techpage(self):
-        response = self.guest_client.get('/about/tech/')
-        self.assertEqual(response.status_code, settings.OK)
-
-    def test_authorpage(self):
-        response = self.guest_client.get('/about/author/')
-        self.assertEqual(response.status_code, settings.OK)
-
 
 class PostURLTests(TestCase):
 
